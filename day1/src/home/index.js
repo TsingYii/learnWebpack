e4a6550b76aced0a43b77d96946ebf88a08132fa
yufang <yufang93@qq.com>
2020-08-24 10:13:16 +0800

    1.eslint提示fix

--- day1/src/home/index.js
+++ day1/src/home/index.js
@@ -3,4 +3,5 @@ log('余樾小宝贝')
 document.getElementById('say').innerText = "余樾小宝贝"
 require('../style/common.css')
 require('../style/home.css')
+// eslint-disable-next-line no-unused-vars
 let a = 1;
--- day1/webpack.config.js
+++ day1/webpack.config.js
@@ -1,4 +1,4 @@
-const path = require('path')
+const {path,__dirname} = require('path')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
